e591cc1c592b395d1a33fd3d61c046c719c20f11
EC2 Default User <ec2-user@ip-172-31-91-254.ec2.internal>
2022-11-02 14:58:05 +0000

    add first program bounce.py

--- Work/01_introduction/bounce.py
+++ Work/01_introduction/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+ball_height = 100 
+num_bounce = 0
+
+while num_bounce < 10:
+    num_bounce = num_bounce + 1
+    ball_height = ball_height*3./5.
+    print(round(num_bounce, 4), round(ball_height, 4))
+
